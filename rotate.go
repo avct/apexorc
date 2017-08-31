891488efc21ea630e37ccf004b9c9c98062bc202
Geoffrey J. Teale <geoffrey@avocet.io>
2017-08-31 15:18:35 +0200

    Test numeric archiving algorithm

--- rotate.go
+++ rotate.go
@@ -1,6 +1,11 @@
 package apexorc
 
 import (
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+	"strconv"
 	"sync"
 
 	"github.com/apex/log"
@@ -11,7 +16,7 @@ type CloserHandler interface {
 	Close() error
 }
 
-type ArchiveFunc func(oldPath string) string
+type ArchiveFunc func(oldPath string) error
 
 type RotatingHandler struct {
 	mu       sync.Mutex
@@ -40,11 +45,45 @@ func (h *RotatingHandler) Rotate() error {
 	h.mu.Lock()
 	defer h.mu.Unlock()
 
-	// err := h.handler.Close()
-	// if err != nil {
-	// 	return err
-	// }
-	// h.archiveF(h.path)
-	// h.handler = NewHandler(h.path)
+	err := h.handler.Close()
+	if err != nil {
+		return err
+	}
+	h.archiveF(h.path)
+	h.handler = NewHandler(h.path)
 	return nil
+}
+
+// NumericArchiveF is an ArchiveFunc that archives historic log files
+// with numeric suffixes.  The lower the suffix the more recent the
+// file.
+func NumericArchiveF(oldPath string) error {
+	var newPath string
+
+	dir, fileName := path.Split(oldPath)
+	extension := path.Ext(fileName)
+	prefix := fileName[:len(fileName)-len(extension)]
+
+	if extension == ".orc" {
+		newPath = oldPath + ".1"
+	} else {
+		counter, err := strconv.Atoi(extension[1:])
+		if err != nil {
+			return err
+		}
+		newPath = fmt.Sprintf("%s.%d", filepath.Join(dir, prefix), counter+1)
+	}
+
+	// If the new path doesn't exist, we'll move the old file there and be done!
+	_, err := os.Stat(newPath)
+	if err == nil || !os.IsNotExist(err) {
+		// This block should recursively move all existing logs back one number
+		err = NumericArchiveF(newPath)
+		if err != nil {
+			return err
+		}
+	}
+
+	return os.Rename(oldPath, newPath)
+
 }
--- rotate_test.go
+++ rotate_test.go
@@ -1,26 +1,77 @@
 package apexorc
 
 import (
-	"fmt"
 	"io/ioutil"
 	"os"
-	"path"
 	"path/filepath"
 	"testing"
 
 	"github.com/apex/log"
 )
 
-func messageInORCFile(message string, path string) bool {
-	return true
-}
+func TestNumericArchiveF(t *testing.T) {
+	tmpdir, err := ioutil.TempDir("", "avct-apexorc-test-rotate")
+	if err != nil {
+		t.Fatalf("Error from ioutil.TempDir: %s", err.Error())
+	}
+	defer os.RemoveAll(tmpdir)
+	path := filepath.Join(tmpdir, "testlog.orc")
 
-func testArchiveF(oldPath string) string {
-	dir, fileName := path.Split(oldPath)
-	extension := path.Ext(fileName)
-	prefix := fileName[:len(fileName)-len(extension)]
-	fmt.Printf("Dir: %q, Ext: %q, prefix %q", dir, extension, prefix)
-	return prefix
+	original := []byte{'t', 'e', 's', 't'}
+	err = ioutil.WriteFile(path, original, 0600)
+	if err != nil {
+		t.Fatalf("Error creating tempfile: %s", err.Error())
+	}
+
+	err = NumericArchiveF(path)
+	if err != nil {
+		t.Fatalf("Error archiving: %s", err.Error())
+	}
+	expectedPath := path + ".1"
+	if _, err := os.Stat(expectedPath); os.IsNotExist(err) {
+		t.Fatalf("Expected file rotated to %q, but it doesn't exist", expectedPath)
+	}
+	content, err := ioutil.ReadFile(expectedPath)
+	if err != nil {
+		t.Fatalf("Error reading rotated file: %q", err.Error())
+	}
+	if string(content) != string(original) {
+		t.Errorf("Expected the rotated file to contain the original content, %q, but found %q", original, content)
+	}
+
+	// OK, if we now create and rotate a 2nd file then the
+	// original one should get rotated to .2
+	other := []byte{'o', 't', 'h', 'e', 'r'}
+	err = ioutil.WriteFile(path, other, 0600)
+	if err != nil {
+		t.Fatalf("Error creating tempfile: %s", err.Error())
+	}
+	err = NumericArchiveF(path)
+	if err != nil {
+		t.Fatalf("Error archiving: %s", err.Error())
+	}
+	// The new file should now be .1
+	if _, err := os.Stat(expectedPath); os.IsNotExist(err) {
+		t.Fatalf("Expected file rotated to %q, but it doesn't exist", expectedPath)
+	}
+	content, err = ioutil.ReadFile(expectedPath)
+	if err != nil {
+		t.Fatalf("Error reading rotated file: %q", err.Error())
+	}
+	if string(content) != string(other) {
+		t.Errorf("Expected the 1st rotated file to contain the content, %q, but found %q", other, content)
+	}
+
+	// And there should now be a .2 file, with the original content
+	expectedPath = path + ".2"
+	content, err = ioutil.ReadFile(expectedPath)
+	if err != nil {
+		t.Fatalf("Error reading rotated file: %q", err.Error())
+	}
+	if string(content) != string(original) {
+		t.Errorf("Expected the 2nd rotated file to contain the content, %q, but found %q", original, content)
+	}
+
 }
 
 // The RotatingHandler passes through HandleLog requests to a
@@ -32,7 +83,7 @@ func TestRotatingHandler(t *testing.T) {
 	}
 	defer os.RemoveAll(tmpdir)
 	path := filepath.Join(tmpdir, "testlog.orc")
-	rotator := NewRotatingHandler(path, testArchiveF)
+	rotator := NewRotatingHandler(path, NumericArchiveF)
 	log.SetHandler(rotator)
 	log.Info("Test 1")
 	err = rotator.Rotate()
